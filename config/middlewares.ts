b3809f04682931a3bfd1d92858ab5c4b57f940dd
lexiVerse <155307560+AhmadPortfoilio@users.noreply.github.com>
2026-07-25 22:41:53 -0700

    Refactor CORS middleware to use object configuration
    Updated CORS middleware configuration to allow specific origins and methods.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,20 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://127.0.0.1:5500',
+        'http://localhost:5500',
+        'https://designbypixantra.com',
+        'https://www.designbypixantra.com',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
